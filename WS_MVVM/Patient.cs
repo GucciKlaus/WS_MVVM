ce3239f70400d29a72fa81fe8e686e85345c43cc
Klaus <klaus.hoehfurtner@gmail.com>
2024-03-04 14:23:14 +0100

    Add project files.

---
+++ WS_MVVM/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS_MVVM
+{
+    public class Patient
+    {
+        public int PatientID { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public bool IsMale { get; set; }
+        public DateTime Birthday { get; set; }
+        public override string ToString()
+        {
+            return $"{Firstname} {Lastname} [{PatientID}] {Birthday.ToShortDateString()}";
+        }
+    }
+}
---
+++ WS_MVVM/PatientViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS_MVVM
+{
+    public class PatientViewModel:ObservableObject
+    {
+        public PatientViewModel()
+        {
+            Patients = new List<Patient>();
+            Init();
+        }
+
+        public PatientViewModel Init() /*Normalerweise hier DBContext mitgeben*/
+        {
+            Patients.Add(new Patient { PatientID = 1,Firstname="Sepp",Lastname="Hubert",Birthday=DateTime.Now.AddYears(-20),IsMale = true});
+            Patients.Add(new Patient { PatientID = 2, Firstname = "Depp", Lastname = "Hubert1", Birthday = DateTime.Now.AddYears(-25), IsMale = true });
+            Patients.Add(new Patient { PatientID = 3, Firstname = "Dreck", Lastname = "Hubert2", Birthday = DateTime.Now.AddYears(-30), IsMale = true });
+            Patients.Add(new Patient { PatientID = 4, Firstname = "Nega", Lastname = "Hubert3", Birthday = DateTime.Now.AddYears(-35), IsMale = true });
+            Patients.Add(new Patient { PatientID = 5, Firstname = "NegaWeib", Lastname = "Hubert4", Birthday = DateTime.Now.AddYears(-40), IsMale = false });
+            return this;
+        }
+
+		private List<Patient> patients;
+
+		public List<Patient> Patients
+		{
+			get { return patients; }
+            set { patients = value;
+                RaisePropertyChangedEvent(nameof(Patients));
+            }
+		}
+
+        private Patient selectedPatient;
+        public Patient SelectedPatient
+        { 
+            get { return selectedPatient; }
+            set { selectedPatient = value;
+                RaisePropertyChangedEvent(nameof(selectedPatient));
+            }
+        }
+    }
+}
